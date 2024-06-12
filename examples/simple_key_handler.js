58e3d4903382d0f5d0c121a72326b54bd54ec9c3
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-06-12 15:07:36 +0200

    Adapt to upstream changes
    * <https://gerrit.libreoffice.org/c/core/+/168700> "Embind: Centrally initialize
      via Module.initUno() in a new uno.js"
    * <https://gerrit.libreoffice.org/c/core/+/168732> "Embind: Let unoObject return
      a css.uno.XInterface reference"

--- examples/simple_key_handler.js
+++ examples/simple_key_handler.js
@@ -7,83 +7,17 @@ let css, xModel, xModel_from_component, xController, refXKeyHandler;
 let evtPressed, evtReleased;
 
 
-//TODO: copied from <https://git.libreoffice.org/core> unotest/source/embindtest/embindtest.js for
-// now:
-const unoObject = function(interfaces, obj) {
-    interfaces = ['com.sun.star.lang.XTypeProvider'].concat(interfaces);
-    obj.impl_refcount = 0;
-    obj.impl_types = new Module.uno_Sequence_type(
-        interfaces.length, Module.uno_Sequence.FromSize);
-    for (let i = 0; i !== interfaces.length; ++i) {
-        obj.impl_types.set(i, Module.uno_Type.Interface(interfaces[i]));
-    }
-    obj.impl_implementationId = new Module.uno_Sequence_byte([]);
-    obj.queryInterface = function(type) {
-        for (const i in obj._types) {
-            if (i === type.toString()) {
-                return new Module.uno_Any(
-                    type,
-                    Module['uno_Type_' + i.replace(/\./g, '$')].reference(
-                        obj._impl[obj._types[i]]));
-            }
-        }
-        return new Module.uno_Any(Module.uno_Type.Void(), undefined);
-    };
-    obj.acquire = function() { ++obj.impl_refcount; };
-    obj.release = function() {
-        if (--obj.impl_refcount === 0) {
-            for (const i in obj._impl) {
-                i.delete();
-            }
-            obj.impl_types.delete();
-            obj.impl_implementationId.delete();
-        }
-    };
-    obj.getTypes = function() { return obj.impl_types; };
-    obj.getImplementationId = function() { return obj.impl_implementationId; };
-    obj._impl = {};
-    interfaces.forEach((i) => {
-        obj._impl[i] = Module['uno_Type_' + i.replace(/\./g, '$')].implement(obj);
-    });
-    obj._types = {};
-    const walk = function(td, impl) {
-        const name = td.getName();
-        if (!Object.hasOwn(obj._types, name)) {
-            if (td.getTypeClass() != css.uno.TypeClass.INTERFACE) {
-                throw new Error('not a UNO interface type: ' + name);
-            }
-            obj._types[name] = impl;
-            const bases = css.reflection.XInterfaceTypeDescription2.query(td).getBaseTypes();
-            for (let i = 0; i !== bases.size(); ++i) {
-                walk(bases.get(i), impl)
-            }
-            bases.delete();
-        }
-    };
-    const tdmAny = Module.getUnoComponentContext().getValueByName(
-        '/singletons/com.sun.star.reflection.theTypeDescriptionManager');
-    const tdm = css.container.XHierarchicalNameAccess.query(tdmAny.get());
-    interfaces.forEach((i) => {
-        const td = tdm.getByHierarchicalName(i);
-        walk(css.reflection.XTypeDescription.query(td.get()), i);
-        td.delete();
-    })
-    tdmAny.delete();
-    obj._types['com.sun.star.uno.XInterface'] = 'com.sun.star.lang.XTypeProvider';
-    obj.acquire();
-    return obj;
-};
-
 function demo() {
     console.log('PLUS: execute example code');
 
-    css = init_unoembind_uno(Module).com.sun.star;
+    Module.initUno();
+    css = Module.uno.com.sun.star;
 
     /* Implements com.sun.star.awt.XKeyHandler
      * Outputs printable characters typed into the OfficeDocument.
      * Browser console is used for output.
      */
-    const myXKeyHandler = unoObject(
+    const myXKeyHandler = Module.unoObject(
         ['com.sun.star.awt.XKeyHandler'],
         {
             keyPressed(e) {
@@ -107,8 +41,7 @@ function demo() {
     }
     xController = xModel.getCurrentController();
 
-    refXKeyHandler = css.awt.XKeyHandler.reference(css.awt.XKeyHandler.implement(myXKeyHandler));
-    xController.addKeyHandler(refXKeyHandler);
+    xController.addKeyHandler(myXKeyHandler);
     // addKeyListener != addKeyHandler (that's something very DIFFERENT)
 
     // Disabled, so variables can be accessed from console.
