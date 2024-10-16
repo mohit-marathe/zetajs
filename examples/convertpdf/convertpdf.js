67cf8452b26a3b097ffb9d435c90669cf670a0e9
Moritz Duge <moritz.duge@allotropia.de>
2024-10-16 22:06:15 +0200

    simplify convertpdf example debugging
    Enable usage of LOWA builds with UI.

--- examples/convertpdf/convertpdf.js
+++ examples/convertpdf/convertpdf.js
@@ -2,30 +2,35 @@
 
 'use strict';
 
-Module.zetajs.then(function(zetajs) {
-    const css = zetajs.uno.com.sun.star;
-    const context = zetajs.getUnoComponentContext();
-    const desktop = css.frame.Desktop.create(context);
 
-    zetajs.mainPort.onmessage = function (e) {
+// for debugging
+let zetajs, css, context, desktop, bean_hidden, bean_overwrite, bean_pdf_export, e, from, to, doc;
+
+Module.zetajs.then(function(argZetajs) {
+    zetajs = argZetajs;
+    css = zetajs.uno.com.sun.star;
+    context = zetajs.getUnoComponentContext();
+    desktop = css.frame.Desktop.create(context);
+
+    bean_hidden = new css.beans.PropertyValue({Name: 'Hidden', Value: true});
+    bean_overwrite = new css.beans.PropertyValue({Name: 'Overwrite', Value: true});
+    bean_pdf_export = new css.beans.PropertyValue({Name: 'FilterName', Value: 'writer_pdf_Export'});
+
+    zetajs.mainPort.onmessage = function (eArg) {
+        e = eArg;
         switch (e.data.cmd) {
         case 'convert':
             try {
-                const from = e.data.from;
-                const to = e.data.to;
-                const doc = desktop.loadComponentFromURL(
-                    'file://' + from, '_blank', 0,
-                    [new css.beans.PropertyValue({Name: 'Hidden', Value: true})]);
-                doc.storeToURL(
-                    'file://' + to,
-                    [new css.beans.PropertyValue({Name: 'Overwrite', Value: true}),
-                     new css.beans.PropertyValue(
-                         {Name: 'FilterName', Value: 'writer_pdf_Export'})]);
-                if (zetajs.fromAny(doc.queryInterface(zetajs.type.interface(css.util.XCloseable)))
-                    !== undefined)
-                {
+                // Close old doc in advance. Keep doc open afterwards for debugging.
+                if (doc !== undefined &&
+                    zetajs.fromAny(doc.queryInterface(zetajs.type.interface(css.util.XCloseable)))
+                        !== undefined) {
                     doc.close(false);
                 }
+                from = e.data.from;
+                to = e.data.to;
+                doc = desktop.loadComponentFromURL('file://' + from, '_blank', 0, [bean_hidden]);
+                doc.storeToURL( 'file://' + to, [bean_overwrite, bean_pdf_export]);
                 zetajs.mainPort.postMessage({cmd: 'converted', name: e.data.name, from, to});
             } catch (e) {
                 const exc = zetajs.catchUnoException(e);
