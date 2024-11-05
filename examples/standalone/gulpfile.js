mohit-marathe/zetajs

commit 66fca1084737e5b292a375f44d9f13e573de917c

standalone: add NPM Gulp parameters and config file
Also listen only to 127.0.0.1 by default for security reasons.

examples/standalone/gulpfile.js
@@ -4,6 +4,7 @@
 const browserSync = require("browser-sync").create();
 const del = require('del');
 const gulp = require('gulp');
+const argv = require('yargs').argv;
 const beautify = require('gulp-beautify');
 const inject = require('gulp-inject-string');
 const mergeStream = require('merge-stream');
@@ -15,10 +16,29 @@ const mergeStream = require('merge-stream');
  * Do not add/edit/save any files or folders iside this folder. They will be deleted by the gulp tasks.
 */
 const distDir = './public/';
-var soffice_base = 'https://zetaoffice.b-cdn.net/zetaoffice_latest/';
+
+
+
+
+// Variables can be adjusted via command line arguments. Example:
+//   npm run start -- --clean_disabled --port 8080 --browser chromium
+// Overwrites in gulpfile_config.js have priority over command line arguments.
+
+var soffice_base = argv.soffice_base;
+// Set "" for same server. But empty strings are falsy, so check "undefined".
+if (soffice_base === "undefined") soffice_base = 'https://zetaoffice.b-cdn.net/zetaoffice_latest/';
+
+var custom_browser = argv.browser;  // else use default system browser
+var custom_listen = argv.listen || '127.0.0.1';
+var custom_port = argv.port || 3000;
+var clean_disabled = argv.clean_disabled;
+
+
+
 
 // Clean up the dist folder before running any task
 function clean() {
+  if (clean_disabled) return Promise.resolve();
   return del(distDir + '**/*');
 }
 
@@ -66,8 +86,10 @@ function initBrowserSync(done) {
         next();
       }
     },
-    port: 3000,
-    notify: false
+    listen: custom_listen,  // host ip
+    port: custom_port,
+    notify: false,
+    browser: custom_browser
   });
   done();
 }
@@ -90,3 +112,9 @@ exports.watch = gulp.series(dist, watchFiles);
 exports.start = gulp.series(dist, gulp.parallel(watchFiles, initBrowserSync) );
 exports.debug = gulp.series(setDebug, gulp.parallel(compileHTML, compileJS, copyVendors) );
 exports.default = dist;
+
+const fs = require('fs');
+const gulpfile_optional = 'gulpfile_config.js';
+if (fs.existsSync(gulpfile_optional)) {
+  eval(fs.readFileSync(gulpfile_optional)+'');
+}

examples/standalone/gulpfile_config.sample.js
@@ -1,0 +1,32 @@
+//// Usage:
+//// Copy this file to gulpfile_config.js to use it.
+////
+//// gulpfile.js will automatically include gulpfile_config.js if it exists.
+
+
+
+
+//////// SIMPLE OPTIONS
+
+//// When serving everything inlcuding LOWA binaries from one server.
+//// E.g. for testing on localhost / 127.0.0.1.
+////
+//// Load soffice.* files from local public/ folder.
+//// Use with "npm run debug". (other tasks clean the public/ folder)
+soffice_base = '';
+
+//// Disable cleaning the public folder for all tasks.
+clean_disabled = true;
+
+
+
+
+//////// ADVANCED OPTIONS
+
+//// You may modify the debug target.
+// exports.debug = gulp.series(gulp.parallel(compileHTML, compileJS, copyVendors));
+
+//// Pick a custom browser and launch it automatically.
+//custom_browser = "chromium";  // ["google chrome", "firefox"]
+//exports.debug = gulp.series(gulp.parallel(compileHTML, compileJS, copyVendors),
+//    gulp.parallel(watchFiles, initBrowserSync) );

commit 7a6dea5437239a1a5b4635952f244fa871db32fc

standalone: fixed setting soffice_base (missing "typeof")
Broken since commit 66fca1084.

examples/standalone/gulpfile.js
@@ -26,7 +26,7 @@ const distDir = './public/';
 
 var soffice_base = argv.soffice_base;
 // Set "" for same server. But empty strings are falsy, so check "undefined".
-if (soffice_base === "undefined") soffice_base = 'https://zetaoffice.b-cdn.net/zetaoffice_latest/';
+if (typeof soffice_base === "undefined") soffice_base = 'https://zetaoffice.b-cdn.net/zetaoffice_latest/';
 
 var custom_browser = argv.browser;  // else use default system browser
 var custom_listen = argv.listen || '127.0.0.1';