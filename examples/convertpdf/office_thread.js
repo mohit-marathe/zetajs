78a97766518c58af58524d85dd905c3842fb3fd4
Moritz Duge <moritz.duge@allotropia.de>
2024-11-08 00:32:58 +0100

    more indentation fixes

--- examples/convertpdf/office_thread.js
+++ examples/convertpdf/office_thread.js
@@ -1,4 +1,4 @@
-/* -*- Mode: JS; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/* -*- Mode: JS; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; fill-column: 100 -*- */
 // SPDX-License-Identifier: MIT
 
 'use strict';
@@ -8,47 +8,47 @@
 let zetajs, css, context, desktop, bean_hidden, bean_overwrite, bean_pdf_export, e, from, to, doc;
 
 function demo() {
-    context = zetajs.getUnoComponentContext();
-    desktop = css.frame.Desktop.create(context);
+  context = zetajs.getUnoComponentContext();
+  desktop = css.frame.Desktop.create(context);
 
-    bean_hidden = new css.beans.PropertyValue({Name: 'Hidden', Value: true});
-    bean_overwrite = new css.beans.PropertyValue({Name: 'Overwrite', Value: true});
-    bean_pdf_export = new css.beans.PropertyValue({Name: 'FilterName', Value: 'writer_pdf_Export'});
+  bean_hidden = new css.beans.PropertyValue({Name: 'Hidden', Value: true});
+  bean_overwrite = new css.beans.PropertyValue({Name: 'Overwrite', Value: true});
+  bean_pdf_export = new css.beans.PropertyValue({Name: 'FilterName', Value: 'writer_pdf_Export'});
 
-    zetajs.mainPort.onmessage = function (eArg) {
-        e = eArg;
-        switch (e.data.cmd) {
-        case 'convert':
-            try {
-                // Close old doc in advance. Keep doc open afterwards for debugging.
-                if (doc !== undefined &&
-                    zetajs.fromAny(doc.queryInterface(zetajs.type.interface(css.util.XCloseable)))
-                        !== undefined) {
-                    doc.close(false);
-                }
-                from = e.data.from;
-                to = e.data.to;
-                doc = desktop.loadComponentFromURL('file://' + from, '_blank', 0, [bean_hidden]);
-                doc.storeToURL( 'file://' + to, [bean_overwrite, bean_pdf_export]);
-                zetajs.mainPort.postMessage({cmd: 'converted', name: e.data.name, from, to});
-            } catch (e) {
-                const exc = zetajs.catchUnoException(e);
-                console.log('TODO', zetajs.getAnyType(exc), zetajs.fromAny(exc).Message);
-            }
-            break;
-        default:
-            throw Error('Unknonwn message command ' + e.data.cmd);
+  zetajs.mainPort.onmessage = function (eArg) {
+    e = eArg;
+    switch (e.data.cmd) {
+    case 'convert':
+      try {
+        // Close old doc in advance. Keep doc open afterwards for debugging.
+        if (doc !== undefined &&
+            zetajs.fromAny(doc.queryInterface(zetajs.type.interface(css.util.XCloseable)))
+            !== undefined) {
+          doc.close(false);
         }
+        from = e.data.from;
+        to = e.data.to;
+        doc = desktop.loadComponentFromURL('file://' + from, '_blank', 0, [bean_hidden]);
+        doc.storeToURL( 'file://' + to, [bean_overwrite, bean_pdf_export]);
+        zetajs.mainPort.postMessage({cmd: 'converted', name: e.data.name, from, to});
+      } catch (e) {
+        const exc = zetajs.catchUnoException(e);
+        console.log('TODO', zetajs.getAnyType(exc), zetajs.fromAny(exc).Message);
+      }
+      break;
+    default:
+      throw Error('Unknonwn message command ' + e.data.cmd);
     }
+  }
 
-    zetajs.mainPort.postMessage({cmd: 'start'});
+  zetajs.mainPort.postMessage({cmd: 'start'});
 }
 
 Module.zetajs.then(function(pZetajs) {
-    // initializing zetajs environment
-    zetajs = pZetajs;
-    css = zetajs.uno.com.sun.star;
-    demo();  // launching demo
+  // initializing zetajs environment
+  zetajs = pZetajs;
+  css = zetajs.uno.com.sun.star;
+  demo();  // launching demo
 });
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
+/* vim:set shiftwidth=2 softtabstop=2 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
