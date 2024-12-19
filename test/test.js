e998b3fe916fc4c5ff0efd979c339ebe96d00bc0
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2024-12-19 15:45:40 +0100

    Add e2e test for convertpdf example
    Run with `npm test` in root directory

---
+++ test/test.js
@@ -0,0 +1,22 @@
+// Test the 'convertpdf' example
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless:false, devtools:true});
+    const page = await browser.newPage();
+    await page.goto('http://127.0.0.1:3000');
+
+    // Wait for zetaoffice to load
+    await page.evaluate("Module.uno_main");
+
+    // Convert test/test.odt to pdf and make sure it opens in new tab
+    const download = await page.$("#download");
+    download.click();
+    const input = await page.$('#input');
+    await input.uploadFile('test/test.odt');
+    const newTarget = await browser.waitForTarget(target => target.opener() === page.target());
+    await newTarget.page();
+
+    await browser.close();
+})();
