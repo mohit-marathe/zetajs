70f549ee48b983eb0e0f1cb200e1087b757e1db0
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2024-12-23 11:21:00 +0100

    Disable sandbox for puppeteer

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.goto('http://127.0.0.1:3000');
 
