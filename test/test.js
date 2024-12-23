68b2beb2b558c9502bb968b03eef397fcb3fc7d5
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2024-12-23 11:17:00 +0100

    Run e2e test on each push

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless:false, devtools:true});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://127.0.0.1:3000');
 
