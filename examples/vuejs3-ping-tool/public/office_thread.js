mohit-marathe/zetajs

commit e8ee4970b9191fd06d62104d72782b2023e0803c

name unoUrl variables uniformly

examples/vuejs3-ping-tool/public/office_thread.js
@@ -15,7 +15,7 @@ let zetajs, css;
 // common variables:
 let context, desktop, xModel, toolkit, topwin, ctrl;
 // example specific:
-let urls, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell;
+let unoUrls, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell;
 
 
 function demo() {
@@ -73,7 +73,7 @@ function demo() {
   // topwin.setMenuBar(null) has race conditions on fast networks like localhost.
   ctrl.getFrame().LayoutManager.hideElement("private:resource/menubar/menubar");
 
-  urls = {};
+  unoUrlsAry = {};
   button('bold', '.uno:Bold');
   button('italic', '.uno:Italic');
   button('underline', '.uno:Underline');
@@ -82,7 +82,7 @@ function demo() {
   zetajs.mainPort.onmessage = function (e) {
     switch (e.data.cmd) {
     case 'toggle':
-      dispatch(urls[e.data.id]);
+      dispatch(unoUrlsAry[e.data.id]);
       break;
     case 'ping_result':
       if (ping_line === undefined) {
@@ -123,9 +123,9 @@ function findEmptyRowInCol1(activeSheet) {
   return line;
 }
 
-function button(id, url) {
-  urls[id] = url;
-  const urlObj = transformUrl(url);
+function button(id, unoUrl) {
+  unoUrlsAry[id] = unoUrl;
+  const urlObj = transformUrl(unoUrl);
   const listener = zetajs.unoObject([css.frame.XStatusListener], {
     disposing: function(source) {},
     statusChanged: function(state) {
@@ -136,8 +136,8 @@ function button(id, url) {
   zetajs.mainPort.postMessage({cmd: 'enable', id});
 }
 
-function transformUrl(url) {
-  const ioparam = {val: new css.util.URL({Complete: url})};
+function transformUrl(unoUrl) {
+  const ioparam = {val: new css.util.URL({Complete: unoUrl})};
   css.util.URLTransformer.create(context).parseStrict(ioparam);
   return ioparam.val;
 }
@@ -146,8 +146,8 @@ function queryDispatch(urlObj) {
   return ctrl.queryDispatch(urlObj, '_self', 0);
 }
 
-function dispatch(url) {
-  const urlObj = transformUrl(url);
+function dispatch(unoUrl) {
+  const urlObj = transformUrl(unoUrl);
   queryDispatch(urlObj).dispatch(urlObj, []);
 }
 

examples/standalone/office_thread.js
@@ -15,7 +15,7 @@ let zetajs, css;
 // common variables:
 let context, desktop, xModel, toolkit, topwin, ctrl;
 // example specific:
-let urls;
+let unoUrlsAry;
 
 
 function demo() {
@@ -66,7 +66,7 @@ function demo() {
   // Turn off sidebar:
   dispatch('.uno:Sidebar');
 
-  urls = {};
+  unoUrlsAry = {};
   button('bold', '.uno:Bold');
   button('italic', '.uno:Italic');
   button('underline', '.uno:Underline');
@@ -74,7 +74,7 @@ function demo() {
   zetajs.mainPort.onmessage = function (e) {
     switch (e.data.cmd) {
     case 'toggle':
-      dispatch(urls[e.data.id]);
+      dispatch(unoUrlsAry[e.data.id]);
       break;
     default:
       throw Error('Unknonwn message command ' + e.data.cmd);
@@ -82,9 +82,9 @@ function demo() {
   }
 }
 
-function button(id, url) {
-  urls[id] = url;
-  const urlObj = transformUrl(url);
+function button(id, unoUrl) {
+  unoUrlsAry[id] = unoUrl;
+  const urlObj = transformUrl(unoUrl);
   const listener = zetajs.unoObject([css.frame.XStatusListener], {
     disposing: function(source) {},
     statusChanged: function(state) {
@@ -95,8 +95,8 @@ function button(id, url) {
   zetajs.mainPort.postMessage({cmd: 'enable', id});
 }
 
-function transformUrl(url) {
-  const ioparam = {val: new css.util.URL({Complete: url})};
+function transformUrl(unoUrl) {
+  const ioparam = {val: new css.util.URL({Complete: unoUrl})};
   css.util.URLTransformer.create(context).parseStrict(ioparam);
   return ioparam.val;
 }
@@ -105,8 +105,8 @@ function queryDispatch(urlObj) {
   return ctrl.queryDispatch(urlObj, '_self', 0);
 }
 
-function dispatch(url) {
-  const urlObj = transformUrl(url);
+function dispatch(unoUrl) {
+  const urlObj = transformUrl(unoUrl);
   queryDispatch(urlObj).dispatch(urlObj, []);
 }
 

commit ed1b52fe8486179d8b560a16e6e630954c6d538c

vuejs3-ping-tool: fixed variable name typo (unoUrlsAry)
broken since commit e8ee4970b

examples/vuejs3-ping-tool/public/office_thread.js
@@ -15,7 +15,7 @@ let zetajs, css;
 // common variables:
 let context, desktop, xModel, toolkit, topwin, ctrl;
 // example specific:
-let unoUrls, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell;
+let unoUrlsAry, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell;
 
 
 function demo() {