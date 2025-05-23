mohit-marathe/zetajs

commit 24bd281e3eb99d9030d47d2b685c382dca247530

version 1.2.0: add zetaHelper module and use for two examples
hint: Onclick must be set explicitly, because of script type="module".

examples/vuejs3-ping-tool/public/pre_soffice.js
@@ -1,45 +1,36 @@
 /* -*- Mode: JS; tab-width: 2; indent-tabs-mode: nil; js-indent-level: 2; fill-column: 100 -*- */
 // SPDX-License-Identifier: MIT
 
-'use strict';
+import { ZetaHelperMain } from './assets/vendor/zetajs/zetaHelper.js';
+
 
 // IMPORTANT:
 // Set base URL to the soffice.* files.
 // Use an empty string if those files are in the same directory.
-let soffice_base_url = 'https://cdn.zetaoffice.net/zetaoffice_latest/';
+let soffice_base_url;
 try {
   soffice_base_url = config_soffice_base_url; // May fail. config.js is optional.
 } catch {}
 
 
-let thrPort;     // zetajs thread communication
-let tbDataJs;    // toolbar dataset passed from vue.js for plain JS
-let PingModule;  // Ping module passed from vue.js for plain JS
+let thrPort;               // zetajs thread communication
+let tbDataJs;              // toolbar dataset passed from vue.js for plain JS
+window.PingModule = null;  // Ping module passed from vue.js for plain JS
 let lastDevicePixelRatio = window.devicePixelRatio;
 
 const loadingInfo = document.getElementById('loadingInfo');
 const canvas = document.getElementById('qtcanvas');
 const pingTarget = document.getElementById("ping_target");
+const btnPing = document.getElementById("btnPing");
 
-
-// Debugging note:
-// Switch the web worker in the browsers debug tab to debug code inside uno_scripts.
-var Module = {
-  canvas,
-  uno_scripts: ['./zeta.js', './office_thread.js'],
-  locateFile: function(path, prefix) { return (prefix || soffice_base_url) + path; },
-};
-if (soffice_base_url !== '') {
-  // Must not be set when soffice.js is in the same directory.
-  Module.mainScriptUrlOrBlob = new Blob(
-    ["importScripts('"+soffice_base_url+"soffice.js');"], {type: 'text/javascript'});
-}
+const zHM = new ZetaHelperMain('office_thread.js', {threadJsMode: 'module', soffice_base_url});
 
 
 function jsPassCtrlBar(pTbDataJs) {
   tbDataJs = pTbDataJs;
   console.log('PLUS: assigned tbDataJs');
 }
+window.jsPassCtrlBar = jsPassCtrlBar;  // make it accessible to vue.js
 
 function toggleFormatting(id) {
   setToolbarActive(id, !tbDataJs.active[id]);
@@ -83,7 +74,7 @@ function pingExamples(err, data) {
   }
 }
 
-function btnPing() {
+function btnPingFunc() {
   // Using Ping callback interface.
   let url = pingTarget.value;
   if (!url.startsWith('http')) {
@@ -95,9 +86,10 @@ function btnPing() {
 }
 pingTarget.addEventListener ("keyup", (evt) => {
   if(evt.key === 'Enter') {
-    btnPing();
+    btnPingFunc();
   }
 });
+btnPing.onclick = btnPingFunc;
 
 
 async function get_calc_ping_example_ods() {
@@ -124,18 +116,14 @@ window.onresize = function() {
 };
 
 
-const soffice_js = document.createElement("script");
-soffice_js.src = soffice_base_url + "soffice.js";
-// "onload" runs after the loaded script has run.
-soffice_js.onload = function() {
-  Module.uno_main.then(function(pThrPort) {
+zHM.start(function() {
     // Should run after App.vue has set PingModule but before demo().
     // 'Cross-Origin-Embedder-Policy': Ping seems to work with 'require-corp' without
     //   acutally having CORP on foreign origins.
     //   Also 'credentialless' isn't supported by Safari-18 as of 2024-09.
-    pingInst = new PingModule();
+    pingInst = new window.PingModule();
 
-    thrPort = pThrPort;
+    thrPort = zHM.thrPort;
     thrPort.onmessage = function(e) {
       switch (e.data.cmd) {
       case 'enable':
@@ -144,7 +132,7 @@ soffice_js.onload = function() {
       case 'state':
         setToolbarActive(e.data.id, e.data.state);
         break;
-      case 'ready':
+      case 'ui_ready':
         loadingInfo.style.display = 'none';
         // Trigger resize of the embedded window to match the canvas size.
         // May somewhen be obsoleted by:
@@ -157,7 +145,7 @@ soffice_js.onload = function() {
             pingInst.ping(urls_ary[urls_ary_i], function(err, data) {
               pingExamples(err, data);
             });
-          }, 1000);  // milliseconds
+          }, 6000);  // milliseconds
         });
         break;
       default:
@@ -167,12 +155,8 @@ soffice_js.onload = function() {
 
     get_calc_ping_example_ods().then(function(aryBuf) {
       calc_ping_example_ods = aryBuf;
-      FS.writeFile('/tmp/calc_ping_example.ods', new Uint8Array(calc_ping_example_ods));
+      zHM.FS.writeFile('/tmp/calc_ping_example.ods', new Uint8Array(calc_ping_example_ods));
     });
-  });
-};
-console.log('Loading WASM binaries for ZetaJS from: ' + soffice_base_url);
-// Hint: The global objects "canvas" and "Module" must exist before the next line.
-document.body.appendChild(soffice_js);
+});
 
 /* vim:set shiftwidth=2 softtabstop=2 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

examples/letter-address-tool/gulpfile.js
@@ -22,13 +22,12 @@ const distDir = './public/';
 // Overwrites in config.js have priority over command line arguments.
 
 var soffice_base_url = argv.soffice_base_url;
-// Set "" for same server. But empty strings are falsy, so check "undefined".
-if (typeof soffice_base_url === "undefined") soffice_base_url = 'https://cdn.zetaoffice.net/zetaoffice_latest/';
 
 var custom_browser = argv.browser;  // else use default system browser
 var custom_listen = argv.listen || '127.0.0.1';
 var custom_port = argv.port || 3000;
 var clean_disabled = argv.clean_disabled;
+
 
 // Clean up the dist folder before running any task
 function clean() {
@@ -36,16 +35,22 @@ function clean() {
   return del(distDir + '**/*');
 }
 
+
 // Task: Compile HTML
 function compileHTML() {
   let css_links ='<link href="assets/vendor/w3/w3.css" rel="stylesheet">';
 
+  // Set "" for same server. But empty strings are falsy, so check "undefined".
+  // If undefined, it's set to null without quotes to let zetaHelper set the URL.
+  if (typeof soffice_base_url !== "undefined") soffice_base_url = "'" + soffice_base_url + "'";
+
   return gulp.src(['index.html'])
     .pipe( inject.replace('<!-- Vendor CSS Files -->', css_links) )
-    .pipe( inject.replace('<!-- soffice.js Base -->', soffice_base_url) )
+    .pipe( inject.replace("'<!-- soffice.js Base -->'", soffice_base_url) )
     .pipe(gulp.dest(distDir))
     .pipe(browserSync.stream());
 }
+
 
 // Task: Compile JS
 function compileJS() {
@@ -55,17 +60,20 @@ function compileJS() {
     .pipe(browserSync.stream());
 }
 
+
 // Task: Copy Vendors
 function copyVendors() {
   let stream = mergeStream();
 
   stream.add( gulp.src( 'node_modules/zetajs/source/zeta.js' ).pipe( gulp.dest( distDir + 'assets/vendor/zetajs/' ) ) );
+  stream.add( gulp.src( 'node_modules/zetajs/source/zetaHelper.js' ).pipe( gulp.dest( distDir + 'assets/vendor/zetajs/' ) ) );
   stream.add( gulp.src( 'node_modules/w3-css/3/w3.css' ).pipe( gulp.dest( distDir + 'assets/vendor/w3/' ) ) );
 
   stream.add( gulp.src( 'Modern_business_letter_sans_serif.ott' ).pipe( gulp.dest( distDir + 'assets/' ) ) );
 
   return stream;
 }
+
 
 // Init live server browser sync
 function initBrowserSync(done) {
@@ -87,16 +95,19 @@ function initBrowserSync(done) {
   done();
 }
 
+
 // Watch files
 function watchFiles() {
   gulp.watch('*.html', compileHTML);
   gulp.watch('*.js', compileJS);
 }
 
+
 function setDebug(done) {
   soffice_base_url = '';
   done();
 }
+
 
 // Export tasks
 const dist = gulp.series(clean, gulp.parallel(compileHTML, compileJS, copyVendors) );

examples/letter-address-tool/index.html
@@ -44,7 +44,7 @@
         <td>
           <h1>ZetaJS Demo: Letter Address Tool</h1>
           <!-- Set height and width. Else insetting buttons changes the size of the page. -->
-          <p style="font-family:monospace"><button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnBold" onclick="formatBtn(this)" style="height:35px; width:35px;" disabled><b>B</b></button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnItalic" onclick="formatBtn(this)" style="height:35px; width:35px;" disabled><i>I</i></button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnUnderline" onclick="formatBtn(this)" style="height:35px; width:35px;" disabled><u>U</u></button></p>
+          <p style="font-family:monospace"><button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnBold" style="height:35px; width:35px;" disabled><b>B</b></button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnItalic" style="height:35px; width:35px;" disabled><i>I</i></button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnUnderline" style="height:35px; width:35px;" disabled><u>U</u></button></p>
         </td>
       </tr>
       <tr>
@@ -71,10 +71,10 @@
         </td>
         <td style="vertical-align: top; width:250px">
           <div class="w3-margin-bottom">
-            Download: <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnOdt" onclick="btnDownloadFunc(this)" disabled>ODT</button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnPdf" onclick="btnDownloadFunc(this)" disabled>PDF</button>
+            Download: <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnOdt" disabled>ODT</button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnPdf" disabled>PDF</button>
           </div>
           <div class="w3-margin-bottom">
-            <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnInsert" onclick="btnInsertFunc()" disabled>Insert Address</button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnReload" onclick="btnReloadFunc()" disabled>Reload file</button>
+            <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnInsert" disabled>Insert Address</button> <button class="w3-button w3-round w3-grey w3-padding-small w3-theme" id="btnReload" disabled>Reload file</button>
           </div>
           <div>
             <!-- size="18" looks best with canvas height="450px" in Firefox-128. -->
@@ -83,14 +83,13 @@
         </td>
       </tr>
     </table>
-    <script type="text/javascript">
-      'use strict';
+    <script type="module">
+      import { ZetaHelperMain } from './assets/vendor/zetajs/zetaHelper.js';
 
       // Set base URL to the soffice.* files.
       // Use an empty string if those files are in the same directory.
       let soffice_base_url = '<!-- soffice.js Base -->';
 
-      const canvas = document.getElementById('qtcanvas');
       const addrName = document.getElementById('addrName');
       const loadingInfo = document.getElementById('loadingInfo');
       const btnNamedAry = {
@@ -102,72 +101,42 @@
         Insert: document.getElementById('btnInsert'),
         Reload: document.getElementById('btnReload'),
       };
-
-      // Workaround initial canvas size +2 bug.
-      canvas.height -= 2;
-      canvas.width -= 2;
+      const zHM = new ZetaHelperMain('office_thread.js', {soffice_base_url});
 
 
-      var Module = {
-        canvas,
-        uno_scripts: ['./assets/vendor/zetajs/zeta.js', './office_thread.js'],
-        locateFile: function(path, prefix) { return (prefix || soffice_base_url) + path; },
-      };
-      if (soffice_base_url !== '') {
-        // Must not be set when soffice.js is in the same directory.
-        Module.mainScriptUrlOrBlob = new Blob(
-            ["importScripts('"+soffice_base_url+"soffice.js');"], {type: 'text/javascript'});
-      }
-
-      let thrPort;     // zetajs thread communication
-      let lastDevicePixelRatio = window.devicePixelRatio;
-
-
-      function formatBtn(btnObj) {
-        thrPort.postMessage({
+      function formatBtnFunc(btnObj) {
+        zHM.thrPort.postMessage({
           cmd: 'toggleFormat',
           id: btnObj.id.substring(3),
         });
         btnObj.style.borderStyle = btnObj.style.borderStyle === 'inset' ? '' : 'inset';
       }
+      btnBold.onclick = function(){ formatBtnFunc(btnBold) };
+      btnItalic.onclick = function(){ formatBtnFunc(btnItalic) };
+      btnUnderline.onclick = function(){ formatBtnFunc(btnUnderline) };
 
       function btnDownloadFunc(btnObj) {
-        thrPort.postMessage({cmd: 'download', id: btnObj.id});
+        zHM.thrPort.postMessage({cmd: 'download', id: btnObj.id});
       }
+      btnOdt.onclick = function(){ btnDownloadFunc(btnOdt) };
+      btnPdf.onclick = function(){ btnDownloadFunc(btnPdf) };
 
-      function btnInsertFunc() {
+      btnInsert.onclick = function() {
         if (addrName.selectedIndex != -1) {
           const recipient = data[addrName.selectedIndex];
-          thrPort.postMessage({cmd: 'insert_address', recipient});
+          zHM.thrPort.postMessage({cmd: 'insert_address', recipient});
         }
-      }
+      };
 
-      function btnReloadFunc() {
+      btnReload.onclick = function() {
         for (const [_, btn] of Object.entries(btnNamedAry)) btn.disabled = true;
-        thrPort.postMessage({cmd: 'reload'});
+        zHM.thrPort.postMessage({cmd: 'reload'});
       }
 
       async function load_business_letter_sans() {
         const response = await fetch("./assets/Modern_business_letter_sans_serif.ott");
         return response.arrayBuffer();
       }
-
-      window.onresize = function() {
-        // Workaround to inform Qt5 about changed browser zoom.
-        setTimeout(function() {
-          if (lastDevicePixelRatio) {
-            if (lastDevicePixelRatio != window.devicePixelRatio) {
-              lastDevicePixelRatio = false;
-              canvas.style.width = parseInt(canvas.style.width) + 1 + 'px';
-              window.dispatchEvent(new Event('resize'));
-            }
-          } else {
-            lastDevicePixelRatio = window.devicePixelRatio
-            canvas.style.width = parseInt(canvas.style.width) - 1 + 'px';
-            window.dispatchEvent(new Event('resize'));
-          }
-        }, 100);
-      };
 
 
       const data = [
@@ -355,53 +324,45 @@
       }
 
 
-      const soffice_js = document.createElement("script");
-      soffice_js.src = soffice_base_url + "soffice.js";
-      // "onload" runs after the loaded script has run.
-      soffice_js.onload = function() {
-        console.log('PLUS: Configuring Module');
-        Module.uno_main.then(function(pThrPort) {
-          thrPort = pThrPort;
-          thrPort.onmessage = function(e) {
-            switch (e.data.cmd) {
-            case 'ready':
-              loadingInfo.style.display = 'none';
-              for (const [_, btn] of Object.entries(btnNamedAry)) btn.disabled = false;
-              // Trigger resize of the embedded window to match the canvas size.
-              // May somewhen be obsoleted by:
-              //   https://gerrit.libreoffice.org/c/core/+/174040
-              window.dispatchEvent(new Event('resize'));
-              break;
-            case 'download':
-              const bytes = FS.readFile('/tmp/output');
-              const format = e.data.id === 'btnOdt' ? 'odt' : 'pdf';
-              const blob = new Blob([bytes], {type: 'application/' + format});
-              const link = document.createElement('a');
-              link.href = URL.createObjectURL(blob);
-              link.download = 'letter.' + format;
-              link.style = 'display:none';
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-              URL.revokeObjectURL(link.href);
-              break;
-            case 'setFormat':
-              btnNamedAry[e.data.id].style.borderStyle = e.data.state ? 'inset' : '';
-              break;
-            default:
-              throw Error('Unknown message command ' + e.data.cmd);
-            }
-          };
-
-          load_business_letter_sans().then(function(aryBuf) {
-            FS.writeFile('/tmp/Modern_business_letter_sans_serif.ott',
-                new Uint8Array(aryBuf));
-          });
-        });
-      };
-      console.log('Loading WASM binaries for ZetaJS from: ' + soffice_base_url);
-      // Hint: The global objects "canvas" and "Module" must exist before the next line.
-      document.body.appendChild(soffice_js);
+      zHM.start(function() {
+        zHM.thrPort.onmessage = function(e) {
+          switch (e.data.cmd) {
+          case 'thr_running':
+            load_business_letter_sans().then(function(aryBuf) {
+              zHM.FS.writeFile('/tmp/Modern_business_letter_sans_serif.ott',
+                  new Uint8Array(aryBuf));
+              zHM.thrPort.postMessage({cmd: 'file_provided'});
+            });
+            break;
+          case 'ui_ready':
+            loadingInfo.style.display = 'none';
+            for (const [_, btn] of Object.entries(btnNamedAry)) btn.disabled = false;
+            // Trigger resize of the embedded window to match the canvas size.
+            // May somewhen be obsoleted by:
+            //   https://gerrit.libreoffice.org/c/core/+/174040
+            window.dispatchEvent(new Event('resize'));
+            break;
+          case 'download':
+            const bytes = zHM.FS.readFile('/tmp/output');
+            const format = e.data.id === 'btnOdt' ? 'odt' : 'pdf';
+            const blob = new Blob([bytes], {type: 'application/' + format});
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'letter.' + format;
+            link.style = 'display:none';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(link.href);
+            break;
+          case 'setFormat':
+            btnNamedAry[e.data.id].style.borderStyle = e.data.state ? 'inset' : '';
+            break;
+          default:
+            throw Error('Unknown message command ' + e.data.cmd);
+          }
+        };
+      });
     </script>
   </body>
 </html>

examples/letter-address-tool/office_thread.js
@@ -5,6 +5,7 @@
 // Switch the web worker in the browsers debug tab to debug this code.
 // It's the "em-pthread" web worker with the most memory usage, where "zetajs" is defined.
 
+// JS mode: script
 'use strict';
 
 
@@ -13,37 +14,24 @@ let zetajs, css;
 
 // = global variables (some are global for easier debugging) =
 // common variables:
-let context, desktop, xModel, toolkit, topwin, ctrl;
+let zHT, context, desktop, xModel, toolkit, topwin, ctrl;
 
 
 function demo() {
-  context = zetajs.getUnoComponentContext();
   const bean_overwrite = new css.beans.PropertyValue({Name: 'Overwrite', Value: true});
   const bean_odt_export = new css.beans.PropertyValue({Name: 'FilterName', Value: 'writer8'});
   const bean_pdf_export = new css.beans.PropertyValue({Name: 'FilterName', Value: 'writer_pdf_Export'});
+  zHT.configDisableToolbars(["Writer"]);
 
-  // Turn off toolbars:
-  const config = css.configuration.ReadWriteAccess.create(context, 'en-US');
-  const uielems = config.getByHierarchicalName(
-    '/org.openoffice.Office.UI.WriterWindowState/UIElements/States');
-  for (const i of uielems.getElementNames()) {
-    const uielem = uielems.getByName(i);
-    if (uielem.getByName('Visible')) {
-      uielem.setPropertyValue('Visible', false);
-    }
-  }
-  config.commitChanges();
-
-  toolkit = css.awt.Toolkit.create(context);
-  desktop = css.frame.Desktop.create(context);
-  loadFile();
-  // Turn off UI elements.
-  // Permanant settings. Don't run again on a document reload.
-  dispatch('.uno:Sidebar');
-  dispatch('.uno:Ruler');
-
-  zetajs.mainPort.onmessage = function (e) {
+  zHT.thrPort.onmessage = function (e) {
     switch (e.data.cmd) {
+    case 'file_provided':
+      loadFile();
+      // Turn off UI elements.
+      // Permanant settings. Don't run again on a document reload.
+      zHT.dispatch(ctrl, context, '.uno:Sidebar');
+      zHT.dispatch(ctrl, context, '.uno:Ruler');
+      break;
     case 'download':
       const format = e.data.id === 'btnOdt' ? bean_odt_export : bean_pdf_export;
       xModel.storeToURL( 'file:///tmp/output', [bean_overwrite, format]);
@@ -54,7 +42,7 @@ function demo() {
       loadFile();
       break;
     case 'toggleFormat':
-      dispatch('.uno:' + e.data.id);
+      zHT.dispatch(ctrl, context, '.uno:' + e.data.id);
       break;
     case 'insert_address':
       const recipient = e.data.recipient;
@@ -106,6 +94,8 @@ function demo() {
       throw Error('Unknonwn message command ' + e.data.cmd);
     }
   }
+
+  zHT.thrPort.postMessage({cmd: 'thr_running'});
 }
 
 function loadFile() {
@@ -126,7 +116,7 @@ function loadFile() {
         if (!topwin) {
           topwin = toolkit.getActiveTopWindow();
           topwin.FullScreen = true;
-          zetajs.mainPort.postMessage({cmd: 'ready'});
+          zetajs.mainPort.postMessage({cmd: 'ui_ready'});
         }
       },
       windowDeactivated(e) {},
@@ -142,36 +132,25 @@ function loadFile() {
   ctrl.getFrame().LayoutManager.hideElement("private:resource/menubar/menubar");
 
   for (const id of ['Bold', 'Italic', 'Underline']) {
-    const urlObj = transformUrl('.uno:' + id);
+    const urlObj = zHT.transformUrl(context, '.uno:' + id);
     const listener = zetajs.unoObject([css.frame.XStatusListener], {
       disposing: function(source) {},
       statusChanged: function(state) {
         zetajs.mainPort.postMessage({cmd: 'setFormat', id, state: zetajs.fromAny(state.State)});
       }
     });
-    queryDispatch(urlObj).addStatusListener(listener, urlObj);
+    zHT.queryDispatch(ctrl, urlObj).addStatusListener(listener, urlObj);
   }
 }
 
-function transformUrl(unoUrl) {
-  const ioparam = {val: new css.util.URL({Complete: unoUrl})};
-  css.util.URLTransformer.create(context).parseStrict(ioparam);
-  return ioparam.val;
-}
 
-function queryDispatch(urlObj) {
-  return ctrl.queryDispatch(urlObj, '_self', 0);
-}
-
-function dispatch(unoUrl) {
-  const urlObj = transformUrl(unoUrl);
-  queryDispatch(urlObj).dispatch(urlObj, []);
-}
-
-Module.zetajs.then(function(pZetajs) {
-  // initializing zetajs environment:
-  zetajs = pZetajs;
-  css = zetajs.uno.com.sun.star;
+import('./assets/vendor/zetajs/zetaHelper.js').then(zetaHelper => {
+  zHT = new zetaHelper.ZetaHelperThread();
+  zetajs = zHT.zetajs;
+  css = zHT.css;
+  context = zHT.context;
+  desktop = zHT.desktop;
+  toolkit = zHT.toolkit;
   demo();  // launching demo
 });
 

examples/vuejs3-ping-tool/public/assets/vendor/zetajs/zeta.js
@@ -1,0 +1,1 @@
+../../../../../../source/zeta.js

examples/vuejs3-ping-tool/public/assets/vendor/zetajs/zetaHelper.js
@@ -1,0 +1,1 @@
+../../../../../../source/zetaHelper.js

examples/vuejs3-ping-tool/public/office_thread.js
@@ -5,35 +5,33 @@
 // Switch the web worker in the browsers debug tab to debug this code.
 // It's the "em-pthread" web worker with the most memory usage, where "zetajs" is defined.
 
-'use strict';
+// JS mode: module
+import { ZetaHelperThread } from './assets/vendor/zetajs/zetaHelper.js';
 
 
 // global variables - zetajs environment:
-let zetajs, css;
+const zHT = new ZetaHelperThread();
+const zetajs = zHT.zetajs;
+const css = zHT.css;
+const context = zHT.context;
+const desktop = zHT.desktop;
+const toolkit = zHT.toolkit;
 
-// = global variables (some are global for easier debugging) =
+// = global variables =
 // common variables:
-let context, desktop, xModel, toolkit, topwin, ctrl;
+let xModel, topwin, ctrl;
 // example specific:
 let unoUrlsAry, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell;
 
+// export variables for debugging
+// Available for debugging via:
+//   globalThis.zetajsStore.threadJsContext
+export { zHT, xModel, topwin, ctrl, unoUrlsAry, ping_line, xComponent, charLocale, formatNumber, formatText, activeSheet, cell };
+
 
 function demo() {
-  context = zetajs.getUnoComponentContext();
+  zHT.configDisableToolbars(["Calc"]);
 
-  // Turn off toolbars:
-  const config = css.configuration.ReadWriteAccess.create(context, 'en-US');
-  const uielems = config.getByHierarchicalName(
-    '/org.openoffice.Office.UI.CalcWindowState/UIElements/States');
-  for (const i of uielems.getElementNames()) {
-    const uielem = uielems.getByName(i);
-    if (uielem.getByName('Visible')) {
-      uielem.setPropertyValue('Visible', false);
-    }
-  }
-  config.commitChanges();
-
-  toolkit = css.awt.Toolkit.create(context);
   // css.awt.XExtendedToolkit::getActiveTopWindow only becomes non-null asynchronously, so wait
   // for it if necessary.
   // addTopWindowListener only works as intended when the following loadComponentFromURL sets
@@ -50,13 +48,12 @@ function demo() {
         if (!topwin) {
           topwin = toolkit.getActiveTopWindow();
           topwin.FullScreen = true;
-          zetajs.mainPort.postMessage({cmd: 'ready'});
+          zHT.thrPort.postMessage({cmd: 'ui_ready'});
         }
       },
       windowDeactivated(e) {},
     }));
 
-  desktop = css.frame.Desktop.create(context);
   xModel = desktop.loadComponentFromURL('file:///tmp/calc_ping_example.ods', '_default', 0, []);
   ctrl = xModel.getCurrentController();
   xComponent = ctrl.getModel();
@@ -67,8 +64,8 @@ function demo() {
     queryKey('@', charLocale, false);
 
   // Turn off UI elements:
-  dispatch('.uno:Sidebar');
-  dispatch('.uno:InputLineVisible');  // FormulaBar at the top
+  zHT.dispatch(ctrl, context, '.uno:Sidebar');
+  zHT.dispatch(ctrl, context, '.uno:InputLineVisible');  // FormulaBar at the top
   ctrl.getFrame().LayoutManager.hideElement("private:resource/statusbar/statusbar");
   // topwin.setMenuBar(null) has race conditions on fast networks like localhost.
   ctrl.getFrame().LayoutManager.hideElement("private:resource/menubar/menubar");
@@ -79,10 +76,10 @@ function demo() {
   button('underline', '.uno:Underline');
 
   activeSheet = ctrl.getActiveSheet();
-  zetajs.mainPort.onmessage = function (e) {
+  zHT.thrPort.onmessage = function (e) {
     switch (e.data.cmd) {
     case 'toggle':
-      dispatch(unoUrlsAry[e.data.id]);
+      zHT.dispatch(ctrl, context, unoUrlsAry[e.data.id]);
       break;
     case 'ping_result':
       if (ping_line === undefined) {
@@ -125,37 +122,17 @@ function findEmptyRowInCol1(activeSheet) {
 
 function button(id, unoUrl) {
   unoUrlsAry[id] = unoUrl;
-  const urlObj = transformUrl(unoUrl);
+  const urlObj = zHT.transformUrl(context, unoUrl);
   const listener = zetajs.unoObject([css.frame.XStatusListener], {
     disposing: function(source) {},
     statusChanged: function(state) {
-      zetajs.mainPort.postMessage({cmd: 'state', id, state: zetajs.fromAny(state.State)});
+      zHT.thrPort.postMessage({cmd: 'state', id, state: zetajs.fromAny(state.State)});
     }
   });
-  queryDispatch(urlObj).addStatusListener(listener, urlObj);
-  zetajs.mainPort.postMessage({cmd: 'enable', id});
+  zHT.queryDispatch(ctrl, urlObj).addStatusListener(listener, urlObj);
+  zHT.thrPort.postMessage({cmd: 'enable', id});
 }
 
-function transformUrl(unoUrl) {
-  const ioparam = {val: new css.util.URL({Complete: unoUrl})};
-  css.util.URLTransformer.create(context).parseStrict(ioparam);
-  return ioparam.val;
-}
-
-function queryDispatch(urlObj) {
-  return ctrl.queryDispatch(urlObj, '_self', 0);
-}
-
-function dispatch(unoUrl) {
-  const urlObj = transformUrl(unoUrl);
-  queryDispatch(urlObj).dispatch(urlObj, []);
-}
-
-Module.zetajs.then(function(pZetajs) {
-  // initializing zetajs environment:
-  zetajs = pZetajs;
-  css = zetajs.uno.com.sun.star;
-  demo();  // launching demo
-});
+demo();  // launching demo
 
 /* vim:set shiftwidth=2 softtabstop=2 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

examples/vuejs3-ping-tool/public/zeta.js
@@ -1,1 +1,0 @@
-../../../source/zeta.js

commit 73b3200649262eab99fb28193cbf819cd7e458ad

vuejs3-ping-tool: fix formatting buttons
broken since commit 24bd281e3

examples/vuejs3-ping-tool/public/pre_soffice.js
@@ -32,7 +32,7 @@ function jsPassCtrlBar(pTbDataJs) {
 }
 window.jsPassCtrlBar = jsPassCtrlBar;  // make it accessible to vue.js
 
-function toggleFormatting(id) {
+window.toggleFormatting = function(id) {  // make it accessible to vue.js
   setToolbarActive(id, !tbDataJs.active[id]);
   thrPort.postMessage({cmd: 'toggle', id});
   // Give focus to the LO canvas to avoid issues with