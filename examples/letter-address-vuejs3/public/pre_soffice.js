mohit-marathe/zetajs

commit 53fa5e1eefeb096e92bd730a577a7b02e8e17bcd

Convert letter-address-vuejs3 example to bootstrap

examples/letter-address-vuejs3/public/pre_soffice.js
@@ -77,28 +77,20 @@ function btnSwitchTab(tab) {
     letterForeground = true;
     btnLetter.classList.add('active');
     btnTable.classList.remove('active');
-    controlbar_row.style.display = null;
+    controlbar.style.display = null;
     btnUpload.accept = '.odt';
     btnInsert.disabled = false;
-    canvasCell.colSpan = 2;
-    canvasCell.classList.remove('tab-address');
     addrNameCell.style.visibility = null;
     addrName.style.visibility = null;
-    canvas.style.height = canvas_height + 'px';
-    canvas.style.width = canvas_width + 'px';
   } else {  // table
     letterForeground = false;
     btnLetter.classList.remove('active');
     btnTable.classList.add('active');
-    controlbar_row.style.display = 'none';
+    controlbar.style.display = 'none';
     btnUpload.accept = '.ods';
     btnInsert.disabled = true;
-    canvasCell.colSpan = 3;
-    canvasCell.classList.add('tab-address');
     addrNameCell.style.visibility = 'hidden';
     addrName.style.visibility = 'hidden';
-    canvas.style.height = canvas_height + 51 + 'px';
-    canvas.style.width = canvas_width + 228 + 'px';
   }
   thrPort.postMessage({cmd: 'switch_tab', id: tab});
 }

examples/letter-address-vuejs3/src/App.vue
@@ -1,204 +1,150 @@
 <!-- SPDX-License-Identifier: MIT -->
 <script setup lang=ts>
-  import ControlBar from '@/components/ControlBar.vue';
+import ControlBar from '@/components/ControlBar.vue';
 </script>
 
 <script lang=ts>
-  export default {
-    mounted() {
-      const config_js = document.createElement("script");
-      config_js.src = "./config.js";
-      document.body.appendChild(config_js);  // May fail. config.js is optional.
-      const pre_soffice_js = document.createElement("script");
-      pre_soffice_js.src = "./pre_soffice.js";
-      pre_soffice_js.onload = function() {
-        ControlBar.init_control_bar();
-      }
-      document.body.appendChild(pre_soffice_js);
-    },
-  };
+export default {
+  mounted() {
+    const config_js = document.createElement("script");
+    config_js.src = "./config.js";
+    document.body.appendChild(config_js);  // May fail. config.js is optional.
+    const pre_soffice_js = document.createElement("script");
+    pre_soffice_js.src = "./pre_soffice.js";
+    pre_soffice_js.onload = function () {
+      ControlBar.init_control_bar();
+    }
+    document.body.appendChild(pre_soffice_js);
+  },
+};
 </script>
 
 <template>
   <div id="app">
-    <h1>ZetaJS: Letter Address Tool</h1>
-
-    <table class="main-table">
-      <tbody>
-        <tr>
-          <td rowspan="2" class="tab-buttons">
-            <button class="w3-button active" id="btnLetter" onclick="btnSwitchTab('letter')" disabled>Letter</button>
-            <button class="w3-button" id="btnTable" onclick="btnSwitchTab('table')" disabled>Addresses</button>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="2" class="upload-section">
-            &nbsp;
-            <label class="w3-button w3-disabled w3-round w3-small w3-padding-small w3-spacing-large w3-theme-border w3-right w3-margin-left" id="lblUpload">
-              Upload File
-              <input accept=".odt" class="w3-round file-input" type="file" id="btnUpload" onchange="btnUploadFunc()" required disabled>
-            </label>
-            &nbsp;
-            <button class="w3-button w3-round w3-small w3-padding-small w3-spacing-large w3-theme-border w3-white w3-right w3-margin-left" id="btnReload" onclick="btnReloadFunc()" disabled>Reload File</button>
-            &nbsp;
-          </td>
-        </tr>
-        <tr class="toolbar-row">
-          <td colspan="3" id="controlbar_row">
-            <ControlBar id="controlbar"/>
-          </td>
-        </tr>
-        <tr class="canvas-row">
-          <td colspan="2" class="canvas-cell" id="canvasCell">
-            <div class="canvas-container" onselectstart="event.preventDefault()">
-              <div id="loadingInfo" class="loading-info">
-                <div class="spinner"></div><br>
-                <h2>ZetaOffice is loading...</h2>
+    <div class="container">
+      <div class="row">
+        <div class="col mt-3 mb-2">
+          <h1>ZetaJS: Letter Address Tool</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="btnLetter" onclick="btnSwitchTab('letter')" type="button" role="tab"
+                aria-controls="home-tab-pane" aria-selected="true">Editor</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="btnTable" onclick="btnSwitchTab('table')" type="button" role="tab"
+                aria-controls="profile-tab-pane" aria-selected="false">Addresses</button>
+            </li>
+          </ul>
+          <div class="tab-content p-1" id="tabContent">
+            <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="btnLetter"
+              tabindex="0">
+              <div class="row">
+                <div id="canvasCell" class="col-lg-9">
+                  <ControlBar id="controlbar" class="mt-1 mb-1" />
+                  <div class="canvas-container" onselectstart="event.preventDefault()">
+                    <div id="loadingInfo" class="loading-info">
+                      <div class="spinner"></div><br>
+                      <h2>ZetaOffice is loading...</h2>
+                    </div>
+                    <canvas id="qtcanvas" contenteditable="true" oncontextmenu="event.preventDefault()"
+                      onkeydown="event.preventDefault()" style="width:100%; height:500px;" class="qt-canvas">
+                      <!-- Qt requires the canvas to have the ID "qtcanvas". -->
+                      <!-- The canvas *must not* have any border or padding, or mouse coords will be wrong. -->
+                      <!-- An outline is fine though. -->
+                    </canvas>
+                  </div>
+                </div>
+                <div class="col-lg-3">
+                  <h4 class="mt-3">Document</h4>
+                  <label class="btn btn-light btn-sm" id="lblUpload">
+                    Upload new file
+                    <input accept=".odt" class="file-input" type="file" id="btnUpload" onchange="btnUploadFunc()">
+                  </label>
+                  <button class="btn btn-light btn-sm ms-2" id="btnReload" onclick="btnReloadFunc()" disabled>Reload
+                    file</button>
+                  <div id="addrNameCell">
+                    <h4 class="mt-3">Recipient</h4>
+                    <select class="form-select" id="addrName" name="addrName" autofocus disabled></select>
+                    <button class="btn btn-primary btn-sm mb-2 mt-2" id="btnInsert" onclick="btnInsertFunc()"
+                      disabled>Insert Address</button>
+                  </div>
+                </div>
               </div>
-              <canvas
-                id="qtcanvas"
-                contenteditable="true"
-                oncontextmenu="event.preventDefault()"
-                onkeydown="event.preventDefault()"
-                style="width:900px; height:500px;"
-                class="qt-canvas">
-                <!-- Qt requires the canvas to have the ID "qtcanvas". -->
-                <!-- The canvas *must not* have any border or padding, or mouse coords will be wrong. -->
-                <!-- An outline is fine though. -->
-              </canvas>
+
             </div>
-          </td>
-          <td class="addr-name-cell" id="addrNameCell">
-            <div>
-              <button class="w3-button w3-round w3-margin-bottom w3-padding-small w3-small w3-theme-border" id="btnInsert" onclick="btnInsertFunc()" disabled>Insert Address</button>
-              <select class="w3-select w3-round w3-light-grey addr-select" id="addrName" size="24" autofocus disabled></select>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <style>
-  .spinner {
-    border: 16px solid #1F2937; /* ZetaOffice brand color */
-    border-top: 16px solid #059669; /* ZetaOffice brand color */
-    border-radius: 50%;
-    width: 120px;
-    height: 120px;
-    position: relative;
-    left: 60px;  /* adjust to center */
-    animation: spin 2s linear 30; /* 60 seconds */
-  }
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
+.spinner {
+  border: 16px solid #1F2937;
+  /* ZetaOffice brand color */
+  border-top: 16px solid #059669;
+  /* ZetaOffice brand color */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  position: relative;
+  left: 60px;
+  /* adjust to center */
+  animation: spin 2s linear 30;
+  /* 60 seconds */
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
 
-  /* Disable dark mode. ControlBar doesn't work well with it. */
-  body {
-    background-color: var(--background-color);
-    color: var(--text-color);
+  100% {
+    transform: rotate(360deg);
   }
+}
 
-  select option {
-    /* Same spacing in select lists for Firefox-128 as for Chromium-130. */
-    padding: 0;
-    height: 18px;
-  }
-  .w3-theme {
-    color: #1F2937 !important; /* ZetaOffice brand color */
-    background-color: #059669 !important; /* ZetaOffice brand color */
-  }
+/* Disable dark mode. ControlBar doesn't work well with it. */
+body {
+  background-color: var(--background-color);
+  color: var(--text-color);
+}
 
-  .w3-theme-border {
-    border: 1px solid #B0B0B0;
-    background-color: white !important;
-  }
+.container {
+  padding: 0;
+}
 
-  .w3-margin-left {
-    margin-left: 8px !important;
-  }
+.file-input {
+  display: none;
+}
 
-  .main-table {
-    width: 1150px;
-    border-spacing: 0px;
-  }
+.canvas-container {
+  position: relative;
+}
 
-  .main-table td {
-    border-color: #B0B0B0;
-    border-style: solid;
-    border-width: 0 0 0 0;
-  }
+.loading-info {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
-  .toolbar-row, .canvas-row {
-    td {
-      padding: 10px;
-    }
-  }
+.qt-canvas {
+  border: 0px none;
+  padding: 0;
+  /*outline: 1px solid #cccccc;*/
+}
 
-  .tab-buttons {
-    white-space: nowrap;
-    width: 1%;
-    border-width: 1px 1px 0 1px !important;
-    padding: 0px !important;
-    button {
-      border-bottom: 3px solid transparent;
-    }
-  }
-
-  .active {
-    border-bottom: 3px solid #059669 !important;
-  }
-
-  .upload-section {
-    border-width: 0 0 1px 0 !important;
-    padding: 0;
-  }
-
-  .file-input {
-    display: none;
-  }
-
-  #controlbar_row {
-    border-width: 0 1px 0 1px !important;
-    padding: 10px 10px 0 10px;
-  }
-
-  .canvas-cell {
-    border-width: 0 0 1px 1px !important;
-  }
-
-  .canvas-cell.tab-address {
-    border-right-width: 1px !important;
-  }
-
-  .canvas-container {
-    position: relative;
-  }
-
-  .loading-info {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  .qt-canvas {
-    border: 0px none;
-    padding: 0;
-    outline: 1px solid #cccccc;
-  }
-
-  .addr-name-cell {
-    vertical-align: top;
-    width: 250px;
-    border-width: 0 1px 1px 0 !important;
-  }
-
-  .addr-select {
-    width: 100%;
-  }
+#tabContent {
+  border: 1px solid var(--bs-border-color);
+  ;
+  border-top: none;
+}
 </style>

examples/letter-address-vuejs3/public/office_thread.js
@@ -230,7 +230,7 @@ function loadFile(fileTab) {
         // Storing the getContainerWindow() result is unstable.
         letterCtrl.getFrame().getContainerWindow().setPosSize(-1000,-1000,500,500,15);
         letterCtrl.getFrame().getContainerWindow().FullScreen = true;
-        
+
         // Get font list for toolbar.
         const fontsUrlObj = transformUrl('.uno:FontNameList');
         const fontsDispatcher = queryDispatch(letterCtrl, fontsUrlObj);

examples/letter-address-vuejs3/src/assets/main.css
@@ -3,9 +3,6 @@
 @import './base.css';
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
   font-weight: normal;
 }
 
@@ -20,18 +17,5 @@ a,
 @media (hover: hover) {
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    /* start: leave free space at the bottom (don't maximize over the whole height) */
-    align-items: start;
-  }
-
-  #app {
-    padding: 0.3rem 0.6rem;  /* border: top left */
   }
 }

examples/letter-address-vuejs3/src/main.js
@@ -1,5 +1,6 @@
-import './assets/w3.css'
 import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 import { createApp } from 'vue'
 import App from './App.vue'

commit 5c08423d8cdc78ba248ef8f00b63c35239ce0579

letter-address-vuejs3: disable buttons on start
w3-disabled broken since commit 53fa5e1ee

examples/letter-address-vuejs3/public/pre_soffice.js
@@ -101,7 +101,7 @@ function btnDownloadFunc(btnId) {
 
 function btnUploadFunc(btnId) {
   for (const elem of disabledElementsAry) elem.disabled = true;
-  lblUpload.classList.add('w3-disabled');
+  lblUpload.classList.add('disabled');
   const filename = letterForeground ? 'letter.odt' : 'table.ods';
   btnUpload.files[0].arrayBuffer().then(aryBuf => {
     FS.writeFile('/tmp/' + filename, new Uint8Array(aryBuf));
@@ -111,7 +111,7 @@ function btnUploadFunc(btnId) {
 
 function btnReloadFunc() {
   for (const elem of disabledElementsAry) elem.disabled = true;
-  lblUpload.classList.add('w3-disabled');
+  lblUpload.classList.add('disabled');
   loadingInfo.style.display = null;
   canvas.style.visibility = 'hidden';
   thrPort.postMessage({cmd: 'reload', id: letterForeground});
@@ -166,7 +166,7 @@ soffice_js.onload = function() {
           canvas.style.visibility = null;
           tbDataJs.font_name_list = e.data.fontsList;
           for (const elem of disabledElementsAry) elem.disabled = false;
-          lblUpload.classList.remove('w3-disabled');
+          lblUpload.classList.remove('disabled');
           btnInsert.disabled = !letterForeground;
         }, 1000);  // milliseconds
         break;

examples/letter-address-vuejs3/src/App.vue
@@ -32,11 +32,11 @@ export default {
           <ul class="nav nav-tabs" role="tablist">
             <li class="nav-item" role="presentation">
               <button class="nav-link active" id="btnLetter" onclick="btnSwitchTab('letter')" type="button" role="tab"
-                aria-controls="home-tab-pane" aria-selected="true">Editor</button>
+                aria-controls="home-tab-pane" aria-selected="true" disabled>Editor</button>
             </li>
             <li class="nav-item" role="presentation">
               <button class="nav-link" id="btnTable" onclick="btnSwitchTab('table')" type="button" role="tab"
-                aria-controls="profile-tab-pane" aria-selected="false">Addresses</button>
+                aria-controls="profile-tab-pane" aria-selected="false" disabled>Addresses</button>
             </li>
           </ul>
           <div class="tab-content p-1" id="tabContent">
@@ -61,9 +61,9 @@ export default {
                 </div>
                 <div class="col-lg-3">
                   <h4 class="mt-3">Document</h4>
-                  <label class="btn btn-light btn-sm" id="lblUpload">
+                  <label class="btn btn-light btn-sm disabled" id="lblUpload">
                     Upload new file
-                    <input accept=".odt" class="file-input" type="file" id="btnUpload" onchange="btnUploadFunc()">
+                    <input accept=".odt" class="file-input" type="file" id="btnUpload" onchange="btnUploadFunc()" disabled>
                   </label>
                   <button class="btn btn-light btn-sm ms-2" id="btnReload" onclick="btnReloadFunc()" disabled>Reload
                     file</button>